b73f2794d7434d0783efb513aa443f3091d26b81
SnowWoolf <88209248+SnowWoolf@users.noreply.github.com>
2026-02-10 14:44:31 +0300

    Update install_kiosk.sh

--- install_kiosk.sh
+++ install_kiosk.sh
@@ -48,20 +48,46 @@ xset s noblank
 unclutter -idle 0 -root &
 
 URL=$(cat /opt/kiosk/url)
+SERVER=$(echo $URL | awk -F/ '{print $3}' | cut -d: -f1)
 
-while true
-do
+open_chrome () {
   chromium \
-  --kiosk "$URL" \
+  --kiosk "$1" \
   --start-fullscreen \
   --noerrdialogs \
   --disable-infobars \
   --disable-session-crashed-bubble \
   --disable-translate \
   --disable-features=TranslateUI \
-  --overscroll-history-navigation=0
+  --overscroll-history-navigation=0 &
+}
 
-  sleep 2
+pkill chromium
+sleep 2
+
+open_chrome "$URL"
+
+STATE="online"
+
+while true
+do
+  if ping -c1 -W1 "$SERVER" >/dev/null 2>&1; then
+      if [ "$STATE" = "offline" ]; then
+          pkill chromium
+          sleep 2
+          open_chrome "$URL"
+          STATE="online"
+      fi
+  else
+      if [ "$STATE" = "online" ]; then
+          pkill chromium
+          sleep 2
+          open_chrome "file:///opt/kiosk/offline.html"
+          STATE="offline"
+      fi
+  fi
+
+  sleep 3
 done
 EOF
 
